970ba679187022354e8dd0258ab73c2af8371fcf
valderer <2055683018@qq.com>
2026-09-19 13:29:31 +0800

    移除遗留 Post 示例文件

--- api/post_api.py
+++
@@ -1,8 +0,0 @@
-class PostApi:
-    """JSONPlaceholder 的文章接口。"""
-
-    def __init__(self, client):
-        self.client = client
-
-    def get_post(self, post_id: int):
-        return self.client.get(f"/posts/{post_id}")
